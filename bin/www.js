29cf850c71b7fc33e423a1dead82e192ae285d80
zahidi14 <d.zahidi1412@gmail.com>
2020-02-08 15:01:48 +0700

    adssasasdasddasd

--- bin/www.js
+++ bin/www.js
@@ -27,7 +27,7 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( './client/build' ));
+  app.use(express.static(path.join(__dirname, 'client', 'build') ));
 
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path

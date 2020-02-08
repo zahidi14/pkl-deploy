66ed531abc26db86577b08c5cfb709a1aeb2fd89
zahidi14 <d.zahidi1412@gmail.com>
2020-02-08 14:28:42 +0700

    adssasasdasddasd

--- bin/www.js
+++ bin/www.js
@@ -89,7 +89,7 @@ function onListening() {
   debug('Listening on ' + bind);
 }
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( '../client/build' ));
+  app.use(express.static( './client/build' ));
 
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path

da0cd54a6e8826a827735b1a74320080fad705a6
zahidi14 <d.zahidi1412@gmail.com>
2020-02-08 15:04:13 +0700

    adssasasdasddasd

--- bin/www.js
+++ bin/www.js
@@ -27,10 +27,10 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build') ));
+  app.use(express.static(path.join(__dirname, './client', 'build') ));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+      res.sendFile(path.join(__dirname, './client', 'build', 'index.html')); // relative path
   });
 }
 server.listen(port);

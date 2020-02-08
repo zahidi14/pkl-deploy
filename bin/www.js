5044ae908a18356a18eaf4c8722236c6216c1939
zahidi14 <d.zahidi1412@gmail.com>
2020-02-08 14:34:14 +0700

    adssasasdasddasd

--- bin/www.js
+++ bin/www.js
@@ -7,6 +7,7 @@
 var app = require('../app');
 var debug = require('debug')('api:server');
 var http = require('http');
+var express = require("express");
 
 /**
  * Get port from environment and store in Express.
@@ -89,7 +90,7 @@ function onListening() {
   debug('Listening on ' + bind);
 }
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( './client/build' ));
+  app.use(express.static( '../client/build' ));
 
   app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
